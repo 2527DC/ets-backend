a7dcd807f3c3294ba0ac526334b4ee6c2f3d9db0
Chethan <chethan962089@gmail.com>
2025-08-30 23:43:33 +0530

    fiex listinging  of project to all network interfacres

--- server.js
+++ server.js
@@ -105,6 +105,6 @@ app.use((req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
